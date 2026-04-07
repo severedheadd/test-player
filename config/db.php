d1d7632daa53575264f9b026d210cc00d5990233
Fedor Medyantsev <teodormed05@gmail.com>
2026-04-08 02:11:14 +0300

    things 3

--- config/db.php
+++ config/db.php
@@ -1,9 +1,27 @@
 <?php
-echo json_encode([
-    "host" => getenv('MYSQLHOST'),
-    "db" => getenv('MYSQLDATABASE'),
-    "user" => getenv('MYSQLUSER'),
-    "port" => getenv('MYSQLPORT')
-]);
-exit;
+require_once "security.php";
+
+$host = getenv('MYSQLHOST');
+$db_name = getenv('MYSQLDATABASE');
+$username = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+$port = getenv('MYSQLPORT');
+
+try {
+    $conn = new PDO(
+        "mysql:host=$host;port=$port;dbname=$db_name;charset=utf8mb4",
+        $username,
+        $password,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]
+    );
+} catch (Exception $e) {
+    echo json_encode([
+        "error" => "DB connection failed",
+        "details" => $e->getMessage()
+    ]);
+    exit;
+}
 ?>
