83f272cfa2d1d35a1a13f4eb923ee16a692c7225
Fedor Medyantsev <teodormed05@gmail.com>
2026-04-08 02:07:20 +0300

    things 2

--- config/db.php
+++ config/db.php
@@ -1,25 +1,9 @@
 <?php
-require_once "security.php";
-
-// получаем переменные окружения
-$host = getenv('MYSQLHOST');
-$db_name = getenv('MYSQLDATABASE');
-$username = getenv('MYSQLUSER');
-$password = getenv('MYSQLPASSWORD');
-$port = getenv('MYSQLPORT');
-
-try {
-    $conn = new PDO(
-        "mysql:host=$host;port=$port;dbname=$db_name;charset=utf8mb4",
-        $username,
-        $password,
-        [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]
-    );
-} catch (Exception $e) {
-    echo json_encode(["error" => "DB connection failed"]);
-    exit;
-}
+echo json_encode([
+    "host" => getenv('MYSQLHOST'),
+    "db" => getenv('MYSQLDATABASE'),
+    "user" => getenv('MYSQLUSER'),
+    "port" => getenv('MYSQLPORT')
+]);
+exit;
 ?>
